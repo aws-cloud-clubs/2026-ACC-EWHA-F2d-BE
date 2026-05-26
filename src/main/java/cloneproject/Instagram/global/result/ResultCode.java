d9bcef100ed685e2b3de07586dbac44bddca1f74
Chaeeun <chaeeun030311@gmail.com>
2026-05-26 18:58:51 +0900

    문법오류 수정

--- src/main/java/cloneproject/Instagram/global/result/ResultCode.java
+++ src/main/java/cloneproject/Instagram/global/result/ResultCode.java
@@ -112,7 +112,7 @@ public enum ResultCode {
 	DELETE_ALL_RECENT_SEARCH_SUCCESS(200, "SE006", "최근 검색 기록 전체 삭제에 성공하였습니다."),
 	GET_MEMBER_AUTO_COMPLETE_SUCCESS(200, "SE007", "멤버 자동완성 조회에 성공하였습니다."),
 	GET_HASHTAG_AUTO_COMPLETE_SUCCESS(200, "SE008", "해시태그 자동완성 조회에 성공하였습니다."),
-	GET_RECOMMEND_MEMBER_SUCCESS(200, "SE009", "팔로잉 추천 멤버 조회에 성공하였습니다.");
+	GET_RECOMMEND_MEMBER_SUCCESS(200, "SE009", "팔로잉 추천 멤버 조회에 성공하였습니다."),
 
 	// 추가
 	// 프리사인URL 발급 성공
