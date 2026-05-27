41d5219db13ab4935e5d2a61837c3f4fdb8bddca
Chaeeun <chaeeun030311@gmail.com>
2026-05-27 09:53:41 +0900

    redis tls 설정 추가

--- src/main/java/cloneproject/Instagram/global/config/RedisConfig.java
+++ src/main/java/cloneproject/Instagram/global/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -18,9 +20,18 @@ public class RedisConfig {
 	@Value("${spring.redis.port}")
 	private int port;
 
+	@Value("${spring.redis.ssl:false}")
+	private boolean ssl;
+
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(host, port);
+		RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(host, port);
+		LettuceClientConfiguration.LettuceClientConfigurationBuilder builder =
+			LettuceClientConfiguration.builder();
+		if (ssl) {
+			builder.useSsl();
+		}
+		return new LettuceConnectionFactory(redisConfig, builder.build());
 	}
 
 	@Bean
